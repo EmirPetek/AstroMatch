673cd33437694e0af817b3393d6750e67d97ab8e
EmirPetek <76615295+EmirPetek@users.noreply.github.com>
2024-07-04 18:01:22 +0300

    login and register screen design

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,6 +53,8 @@ dependencies {
     implementation("com.google.firebase:firebase-database:20.3.1")
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
     implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
+    implementation("androidx.annotation:annotation:1.6.0")
+    implementation("androidx.fragment:fragment-ktx:1.5.6")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
