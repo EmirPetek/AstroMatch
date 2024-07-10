d63962a16997f79e472e983c49877d77793edda9
EmirPetek <76615295+EmirPetek@users.noreply.github.com>
2024-07-11 02:21:25 +0300

    register operations completed.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,5 +59,9 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation ("com.google.dagger:hilt-android:2.50")
+    implementation(platform("com.google.firebase:firebase-bom:33.1.1"))
+    implementation("com.google.firebase:firebase-auth")
+
+
 
 }
--- app/src/main/java/com/emirpetek/mybirthdayreminder/ui/fragment/login/RegisterFragment.kt
+++ app/src/main/java/com/emirpetek/mybirthdayreminder/ui/fragment/login/RegisterFragment.kt
@@ -2,19 +2,27 @@ package com.emirpetek.mybirthdayreminder.ui.fragment.login
 
 import androidx.fragment.app.viewModels
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.navigation.Navigation
 import com.emirpetek.mybirthdayreminder.R
 import com.emirpetek.mybirthdayreminder.databinding.FragmentRegisterBinding
 import com.emirpetek.mybirthdayreminder.viewmodel.RegisterViewModel
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class RegisterFragment : Fragment() {
 
     private val viewModel: RegisterViewModel by viewModels()
     private lateinit var binding: FragmentRegisterBinding
+    private lateinit var auth: FirebaseAuth
+    private val TAG = "RegisterFragment Log"
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -26,8 +34,62 @@ class RegisterFragment : Fragment() {
             Navigation.findNavController(it).popBackStack()
         }
 
+        auth = Firebase.auth
+
+
+
+
+        binding.buttonRegisterSignUp.setOnClickListener {
+
+            val email = binding.editTextRegisterEmail.text.toString()
+            val password = binding.editTextRegisterPassword.text.toString()
+            val passwordAgain = binding.editTextRegisterPasswordAgain.text.toString()
+            var pwCreateUser = ""
+
+            if (email.isEmpty()) Toast.makeText(context,"E posta adresi boş geçilemez", Toast.LENGTH_SHORT).show()
+
+            if (password != passwordAgain) {
+                Toast.makeText(context,"Şifreler eşleşmiyor", Toast.LENGTH_SHORT).show()
+            }else{
+                pwCreateUser = password
+            }
+
+            auth.createUserWithEmailAndPassword(email, pwCreateUser)
+                .addOnCompleteListener(requireActivity()) { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Log.e(TAG, "createUserWithEmail:success")
+                        val user = auth.currentUser
+                        //updateUI(user)
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.e(TAG, "createUserWithEmail:failure", task.exception)
+                        Toast.makeText(
+                            context,
+                            "Bu adrese kayıtlı zaten bir kullanıcı bulunmaktadır.",
+                            Toast.LENGTH_SHORT,
+                        ).show()
+                        //updateUI(null)
+                    }
+
+
+
+                }
+
+        }
 
 
         return binding.root
+    }
+
+    public override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+        if (currentUser != null) {
+            //reload()
+
+            Log.e("state: ", "register fragment onstart if içi")
+        }
     }
 }
