d3db4c9e5baa20e8400c40143d77e8158ee1ae61
EmirPetek <76615295+EmirPetek@users.noreply.github.com>
2024-07-26 00:17:58 +0300

    Settings fragment and its items created

---
+++ app/src/main/java/com/emirpetek/mybirthdayreminder/viewmodel/profile/ProfileSettingsViewModel.kt
@@ -0,0 +1,7 @@
+package com.emirpetek.mybirthdayreminder.viewmodel.profile
+
+import androidx.lifecycle.ViewModel
+
+class ProfileSettingsViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
