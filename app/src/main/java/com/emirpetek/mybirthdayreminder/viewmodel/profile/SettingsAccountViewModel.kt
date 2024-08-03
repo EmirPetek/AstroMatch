244ceba41cc1c3226d7421ae6f60a4d096118de4
EmirPetek <76615295+EmirPetek@users.noreply.github.com>
2024-08-03 15:14:08 +0300

    settings account vm

---
+++ app/src/main/java/com/emirpetek/mybirthdayreminder/viewmodel/profile/SettingsAccountViewModel.kt
@@ -0,0 +1,22 @@
+package com.emirpetek.mybirthdayreminder.viewmodel.profile
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.emirpetek.mybirthdayreminder.data.entity.user.User
+import com.emirpetek.mybirthdayreminder.data.repo.user.UserRepo
+
+class SettingsAccountViewModel : ViewModel() {
+
+    private val userRepo = UserRepo()
+    var userData = MutableLiveData<User>()
+
+    init {
+        userData = userRepo.getUser()
+    }
+
+    fun getUserData(userID:String){
+        userRepo.getUserData(userID)
+    }
+
+}
