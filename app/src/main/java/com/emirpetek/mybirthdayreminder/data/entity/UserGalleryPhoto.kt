f977d381f1b6a57fc97a915c7ef1728ef89f5112
EmirPetek <76615295+EmirPetek@users.noreply.github.com>
2024-08-08 00:07:22 +0300

    user gallery photo parcelable operations

--- app/src/main/java/com/emirpetek/mybirthdayreminder/data/entity/UserGalleryPhoto.kt
+++ app/src/main/java/com/emirpetek/mybirthdayreminder/data/entity/UserGalleryPhoto.kt
@@ -1,10 +1,37 @@
 package com.emirpetek.mybirthdayreminder.data.entity
 
+import android.os.Parcel
+import android.os.Parcelable
 import java.io.Serializable
 
 data class UserGalleryPhoto(
     val imageURL:String,
     val timestamp: Long
-): Serializable{
+): Parcelable{
+    constructor(parcel: Parcel) : this(
+        parcel.readString().toString(),
+        parcel.readLong()
+    ) {
+    }
+
     constructor() : this("",0)
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(imageURL)
+        parcel.writeLong(timestamp)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<UserGalleryPhoto> {
+        override fun createFromParcel(parcel: Parcel): UserGalleryPhoto {
+            return UserGalleryPhoto(parcel)
+        }
+
+        override fun newArray(size: Int): Array<UserGalleryPhoto?> {
+            return arrayOfNulls(size)
+        }
+    }
 }
