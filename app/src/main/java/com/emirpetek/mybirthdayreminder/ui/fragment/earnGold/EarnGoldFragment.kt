e2b601ace9c2b86679ee52934a4de0d98913e790
EmirPetek <76615295+EmirPetek@users.noreply.github.com>
2024-08-20 01:17:18 +0300

    Earn gold fragment remain time to next free gold and right backend completed

--- app/src/main/java/com/emirpetek/mybirthdayreminder/ui/fragment/earnGold/EarnGoldFragment.kt
+++ app/src/main/java/com/emirpetek/mybirthdayreminder/ui/fragment/earnGold/EarnGoldFragment.kt
@@ -1,11 +1,15 @@
 package com.emirpetek.mybirthdayreminder.ui.fragment.earnGold
 
+import android.R
 import androidx.fragment.app.viewModels
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
 import com.emirpetek.mybirthdayreminder.ui.util.bottomNavigation.ManageBottomNavigationVisibility
 import com.emirpetek.mybirthdayreminder.databinding.FragmentEarnGoldBinding
 import com.emirpetek.mybirthdayreminder.viewmodel.earnGold.EarnGoldViewModel
@@ -14,6 +18,7 @@ class EarnGoldFragment : Fragment() {
 
     private val viewModel: EarnGoldViewModel by viewModels()
     private lateinit var binding: FragmentEarnGoldBinding
+    private var timeLeft : Long = 0
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -33,6 +38,45 @@ class EarnGoldFragment : Fragment() {
         binding.cardBuyGold5.setOnClickListener { viewModel.incrementUserCredit(1000) }
         binding.cardBuyGold6.setOnClickListener { viewModel.incrementUserCredit(5000) }
 
+        viewModel.getCreditData()
+        viewModel.credit.observe(viewLifecycleOwner, Observer { credit ->
+            Log.e("veri crt: ", System.currentTimeMillis().toString())
+            Log.e("veri lcbts: ", credit.lastCreditBalanceTimestamp.toString())
+            timeLeft = 86400000 - (System.currentTimeMillis() - credit.lastCreditBalanceTimestamp)
+            Log.e("veri lcbts: ", timeLeft.toString())
+
+            startCountdownTimer(timeLeft,
+                onTick = { timeFormatted ->
+                    binding.textViewEarnGoldRemainTimeFreeGoldLike.text = "$timeFormatted"
+                    // Her saniye kalan zamanı günceller
+                },
+                onFinish = {
+                    println("Time's up!") // Geri sayım bittiğinde çalışacak kod
+                }
+            )
+
+
+        })
+
+
         return binding.root
     }
+
+    fun startCountdownTimer(milliseconds: Long, onTick: (String) -> Unit, onFinish: () -> Unit) {
+        val countDownTimer = object : CountDownTimer(milliseconds, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                val hours = millisUntilFinished / (1000 * 60 * 60)
+                val minutes = (millisUntilFinished / (1000 * 60)) % 60
+                val seconds = (millisUntilFinished / 1000) % 60
+                val timeFormatted = String.format("%02d:%02d:%02d", hours, minutes, seconds)
+                onTick(timeFormatted) // Her saniye güncelleme
+            }
+
+            override fun onFinish() {
+                //onFinish() // Geri sayım bittiğinde yapılacak işlem
+            }
+        }
+        countDownTimer.start()
+    }
+
 }
--- app/src/main/java/com/emirpetek/mybirthdayreminder/viewmodel/earnGold/EarnGoldViewModel.kt
+++ app/src/main/java/com/emirpetek/mybirthdayreminder/viewmodel/earnGold/EarnGoldViewModel.kt
@@ -1,14 +1,25 @@
 package com.emirpetek.mybirthdayreminder.viewmodel.earnGold
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.emirpetek.mybirthdayreminder.data.entity.UserCredits
 import com.emirpetek.mybirthdayreminder.data.repo.CreditsRepo
 
 class EarnGoldViewModel : ViewModel() {
 
     val creditRepo = CreditsRepo()
+    var credit = MutableLiveData<UserCredits>()
+
+    init {
+        credit = creditRepo.getCreditAmount()
+    }
 
     fun incrementUserCredit(amount: Long){
         creditRepo.incrementUserCredit(amount)
+    }
+
+    fun getCreditData(){
+        creditRepo.getUserCreditsAmountFun()
     }
 
 }
