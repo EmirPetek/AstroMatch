8290f419827073bbd2c70547d6c68041c939f0b6
EmirPetek <76615295+EmirPetek@users.noreply.github.com>
2024-08-25 02:49:13 +0300

    ComeLikesFragment banner admob added

--- app/src/main/java/com/emirpetek/mybirthdayreminder/ui/fragment/comeLikes/ComeLikesFragment.kt
+++ app/src/main/java/com/emirpetek/mybirthdayreminder/ui/fragment/comeLikes/ComeLikesFragment.kt
@@ -6,16 +6,25 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.ViewGroup.LayoutParams
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.GridLayoutManager
+import com.emirpetek.mybirthdayreminder.R
 import com.emirpetek.mybirthdayreminder.data.entity.like.Like
 import com.emirpetek.mybirthdayreminder.databinding.FragmentComeLikesBinding
 import com.emirpetek.mybirthdayreminder.ui.adapter.comeLikes.ComeLikesAdapter
 import com.emirpetek.mybirthdayreminder.ui.util.bottomNavigation.ManageBottomNavigationVisibility
 import com.emirpetek.mybirthdayreminder.viewmodel.comeLikes.ComeLikesViewModel
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdSize
+import com.google.android.gms.ads.AdView
+import com.google.android.gms.ads.MobileAds
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class ComeLikesFragment : Fragment() {
 
@@ -23,6 +32,8 @@ class ComeLikesFragment : Fragment() {
     private lateinit var binding: FragmentComeLikesBinding
     private val ownUserID = Firebase.auth.currentUser!!.uid
     private lateinit var adapter : ComeLikesAdapter
+    private lateinit var mAdView : AdView
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -32,6 +43,7 @@ class ComeLikesFragment : Fragment() {
         ManageBottomNavigationVisibility(requireActivity()).hideBottomNav()
         binding.imageViewComeLikesBackButton.setOnClickListener { findNavController().popBackStack() }
 
+        bindAdMob()
 
         binding.progressBarComeLikes.visibility = View.VISIBLE
         viewModel.getLikes(ownUserID)
@@ -56,9 +68,25 @@ class ComeLikesFragment : Fragment() {
                 binding.textViewComeLikesNoLikeHere.visibility = View.VISIBLE
             }
         })
-
-
-
         return binding.root
+    }
+
+    fun bindAdMob(){
+        // comelikes sayfası reklam gösterme kısmı
+        val backgroundScope = CoroutineScope(Dispatchers.IO)
+        backgroundScope.launch {
+            // Initialize the Google Mobile Ads SDK on a background thread.
+            MobileAds.initialize(requireContext()) {}
+        }
+        mAdView = binding.adViewComeLikes
+        val adView = AdView(requireContext())
+        adView.adUnitId = getString(R.string.ad_unit_id)
+        val adSize = AdSize(LayoutParams.MATCH_PARENT, 60)
+        adView.setAdSize(adSize)
+        this.mAdView = adView
+        binding.adViewComeLikes.removeAllViews()
+        binding.adViewComeLikes.addView(adView)
+        val adRequest = AdRequest.Builder().build()
+        adView.loadAd(adRequest)
     }
 }
