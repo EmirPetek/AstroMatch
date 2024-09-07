c0b9bd651b295a5b6e91c59fb521f534d2d132d2
EmirPetek <76615295+EmirPetek@users.noreply.github.com>
2024-09-07 09:51:16 +0300

    FragmentProfileSettings user logout operation completed

--- app/src/main/java/com/emirpetek/mybirthdayreminder/ui/fragment/profile/settings/ProfileSettingsFragment.kt
+++ app/src/main/java/com/emirpetek/mybirthdayreminder/ui/fragment/profile/settings/ProfileSettingsFragment.kt
@@ -6,11 +6,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.NavOptions
 import androidx.navigation.fragment.findNavController
 import com.emirpetek.mybirthdayreminder.R
 import com.emirpetek.mybirthdayreminder.databinding.FragmentProfileSettingsBinding
 import com.emirpetek.mybirthdayreminder.viewmodel.profile.ProfileSettingsViewModel
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class ProfileSettingsFragment : Fragment() {
 
@@ -27,7 +31,14 @@ class ProfileSettingsFragment : Fragment() {
 
         binding.imageViewFragmentSettingsToolbar.setOnClickListener { findNavController().popBackStack() }
         binding.constraintLayoutSettingsAccount.setOnClickListener { findNavController().navigate(R.id.action_profileSettingsFragment_to_settingsAccountFragment) }
-
+        binding.constraintLayoutSettingsLogout.setOnClickListener {
+            FirebaseAuth.getInstance().signOut()
+            findNavController().navigate(
+                R.id.action_profileSettingsFragment_to_loginFragment,
+                null,
+                NavOptions.Builder().setPopUpTo(R.id.loginFragment,true)
+                    .build())//navigate(R.id.action_profileSettingsFragment_to_loginFragment,)
+        }
 
         return binding.root
     }
